4ada774280ca363b97d52abbbb4e7b9b7ecf9f93
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-09-20 11:43:06 -0300

    Atualizado

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  constructor(
+    protected fire: AngularFirestore
+) { }
+
+  save(player){
+    return this.fire.collection("players").add(player)
+  }
 }
+  
