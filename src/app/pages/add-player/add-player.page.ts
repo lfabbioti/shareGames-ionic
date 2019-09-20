86eef36bff94769881744a3267f8d406611004c7
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-09-20 10:50:26 -0300

    Atualizado

--- src/app/pages/add-player/add-player.page.ts
+++ src/app/pages/add-player/add-player.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../../model/player';
 
 @Component({
   selector: 'app-add-player',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPlayerPage implements OnInit {
 
+  protected player: Player = new Player;
   constructor() { }
 
   ngOnInit() {
