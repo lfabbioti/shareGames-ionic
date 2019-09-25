7db0a2c22884e9acf3fd95d2256423ba7ff6c51d
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-09-25 11:32:14 -0300

    Atualizado

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Player } from '../model/player';
-import { map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,28 +9,31 @@ import { map} from 'rxjs/operators';
 export class PlayerService {
 
   constructor(
-    protected fire:AngularFirestore
+    protected fire: AngularFirestore
   ) { }
 
-save(player){
-  return this.fire.collection("players")
-  .add({
-    nome:player.nome,
-    nickname: player.nickname,
-    email:player.email,
-    pws:player.pws,
-    ativo: true
-    }); 
+  save(player) {
+    return this.fire.collection("players")
+      .add({
+        nome: player.nome,
+        nickname: player.nickname,
+        email: player.email,
+        pws: player.pws,
+        ativo: true
+      });
   }
 
-getAll(){
-  return this.fire.collection("players").snapshotChanges()
-  .pipe(
-    map(dados => 
-      dados.map(d=> ({key :d.payload.doc.id, ...d.payload.doc.data() }))
-    )
+  getAll() {
+    return this.fire.collection("players").snapshotChanges()
+      .pipe(
+        map(dados =>
+          dados.map(d => ({ key: d.payload.doc.id, ...d.payload.doc.data() }))
+        )
 
-    )
-}
+      )
+  }
 
+get(id){
+  return this.fire.collection("players").doc<Player>(id).valueChanges();
+  }
 }
