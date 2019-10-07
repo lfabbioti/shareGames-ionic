e93e1a7a090e9866b68f04301a4993e3dc1c2cbe
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-10-07 10:44:31 -0300

    Em andamento

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -5,16 +6,15 @@
 export const environment = {
   production: false,
   firebaseConfig:{
-    apiKey: "AIzaSyAT2m-BIWf3gqJd-g6o1zVP5UaKi-94NjQ",
-    authDomain: "sharegames-acba6.firebaseapp.com",
-    databaseURL: "https://sharegames-acba6.firebaseio.com",
-    projectId: "sharegames-acba6",
+    apiKey: "AIzaSyA0ewiv-QUrBYajFiHVB7iLx6sM__ykSXQ",
+    authDomain: "sharegames-bd.firebaseapp.com",
+    databaseURL: "https://sharegames-bd.firebaseio.com",
+    projectId: "sharegames-bd",
     storageBucket: "",
-    messagingSenderId: "36898137449",
-    appId: "1:36898137449:web:d849f9bf1b9149642309f8"
+    messagingSenderId: "468512495009",
+    appId: "1:468512495009:web:3ff803ef52b8699aeb67e0"
   }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
